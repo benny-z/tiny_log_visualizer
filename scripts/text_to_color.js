e52e9b09fdb69b8eef0c525a889a29414e9052ae
bennyz <bennyz@bennyz.ubuntu>
2021-01-08 11:47:55 +0200

    Switched to quicker hash function. Solved issue with hashing short strings

--- scripts/text_to_color.js
+++ scripts/text_to_color.js
@@ -1,13 +1,20 @@
-function calcHash32(input_str){
-    var hash = 0, i, chr;
-    for (i = 0; i < input_str.length; i++) {
-        chr   = input_str.charCodeAt(i);
-        hash  = ((hash << 5) - hash) + chr;
-        hash |= 0;
-    }
-    return Math.abs(hash >> 16);
+// source: https://github.com/darkskyapp/string-hash
+function hash(str) {
+  let hash = 5381;
+  let i = str.length;
+  while(i) {
+    hash = (hash * 33) ^ str.charCodeAt(--i);
+  }
+  /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
+   * integers. Since we want the results to be always positive, convert the
+   * signed int to an unsigned by doing an unsigned bitshift. */
+  return hash >>> 0;
 }
 
 function str2color(input_str){
-    return calcHash32(input_str);
+	let str = input_str;
+  if (input_str.length < 5) {
+    str += 37 * hash(str);
+  }
+  return hash(str);
 }
